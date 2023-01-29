02291695316a9871cfa29469a3380502cf6dfc41
Ulitin Sergey <ulitin_sergey@mail.ru>
2023-01-29 13:06:05 +0300

    end

---
+++ 2 задание/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+int CalculateSumm(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+    }
+    return (end + start) * (end - start + 1) / 2; // по формуле суммы членов арифметической прогрессии
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+Console.WriteLine(CalculateSumm(m, n));
